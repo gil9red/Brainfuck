fbe7b41a148e5f9d8921150add549526d9178d76
gil9red <ilya.petrash@inbox.ru>
2025-03-21 00:53:40 +0500

    Refactoring

--- Interpreter Brainfuck.py
+++ Interpreter Brainfuck.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = "ipetrash"
+
+
 """
 EN:
 Interpreter language Brainfuck.
@@ -29,29 +35,24 @@ RU:
                         [ (с учётом вложенности)
 """
 
+
 from collections import defaultdict
-import sys
-import argparse
 
 
-__author__ = 'ipetrash'
-
-
-
-def get_loops_block(source):
-    begin_block = []
-    blocks = {}
+def get_loops_block(source: str) -> dict[int, int]:
+    begin_block: list[int] = []
+    blocks: dict[int, int] = dict()
     for i, s in enumerate(source):
-        if s is '[':
+        if s == "[":
             begin_block.append(i)
-        elif s is ']':
+        elif s == "]":
             b_i = begin_block.pop()  # b_i -- begin index
             blocks[i] = b_i
             blocks[b_i] = i
     return blocks
 
 
-def execute(source):
+def execute(source: str):
     """
     EN:
     The function parses source code Brainfuck and execute it.
@@ -65,47 +66,48 @@ def execute(source):
 
     i = 0  # A pointer to the row index in the code
     x = 0  # Cell index
-    bf = defaultdict(int)  # Dictionary, which is stored in the key index of the cell, and in the value - its value
+    # Dictionary, which is stored in the key index of the cell, and in the value - its value
+    bf: dict[int, int] = defaultdict(int)
     l = len(source)  # Number of code symbols
     loops_block = get_loops_block(source)
 
     while i < l:
         s = source[i]
 
-        if s is '>':  # Go to the next cell
+        if s == ">":  # Go to the next cell
             x += 1
-        elif s is '<':  # Go to the previous cell
+        elif s == "<":  # Go to the previous cell
             x -= 1
-        elif s is '+':  # Increasing the value of the current cell on 1
+        elif s == "+":  # Increasing the value of the current cell on 1
             bf[x] += 1
-        elif s is '-':  # Decrease the value of the current cell on 1
+        elif s == "-":  # Decrease the value of the current cell on 1
             bf[x] -= 1
-        elif s is '.':  # Printing the value of the current cell
-            print(chr(bf[x]), end='')
-        elif s is ',':  # Enter a value in the current cell
+        elif s == ".":  # Printing the value of the current cell
+            print(chr(bf[x]), end="")
+        elif s == ",":  # Enter a value in the current cell
             bf[x] = int(input("Input = "))
-        elif s is '[':  # Begin loop
-            if not bf[x]:  # If bf[x] == 0, then gets the index of the closing parenthesis
+        elif s == "[":  # Begin loop
+            # If bf[x] == 0, then gets the index of the closing parenthesis
+            if not bf[x]:
                 i = loops_block[i]
-        elif s is ']':  # End loop
+        elif s == "]":  # End loop
             if bf[x]:  # Если bf[x] != 0, then gets the index of the opening parenthesis
                 i = loops_block[i]
         i += 1
 
 
-def create_parser():
-    parser = argparse.ArgumentParser(description='Interpreter language Brainfuck.')
-    parser.add_argument("path", help="Path to file")
-    return parser
+if __name__ == "__main__":
+    import sys
+    import argparse
 
+    from pathlib import Path
 
-if __name__ == '__main__':
-    parser = create_parser()
+    parser = argparse.ArgumentParser(description="Interpreter language Brainfuck.")
+    parser.add_argument("path", type=Path, help="Path to file")
 
-    if len(sys.argv) is 1:
+    if len(sys.argv) == 1:
         parser.print_help()
     else:
         args = parser.parse_args()
-        file_name = args.path
-        source = open(file_name).read()
+        source = args.path.read_text(encoding="utf-8")
         execute(source)
